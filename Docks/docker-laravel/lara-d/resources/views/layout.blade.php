e9717f6b5a96cc2c848e5457576a8a10591e336f
daiki <daiki.m.0316@gmail.com>
2021-02-05 15:31:58 +0900

    first commit

---
+++ Docks/docker-laravel/lara-d/resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacksレビュー</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+        crossorigin="anonymous">
+</head>
+<body>
+    <header class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="">
+                Hacksレビュー
+            </a>
+        </div>
+    </header>
+
+    <main>
+        <!-- テンプレート -->
+        @yield('content')
+    </main>
+
+    <footer class="footer bg-dark">
+        <div class="container">
+            <p class="text-white text-center">
+                🄫Daiki-Minaguchi All Rights Reserved
+            </p>        
+        </div>
+    </footer>
+</body>
+</html>
---
+++ Docks/docker-laravel/lara-d/resources/views/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div>
+        hello index
+    </div>
+@endsection('content')
